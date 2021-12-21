92c64d9daa01812e5313e426096b6323c45b3931
JJun0718 <51451156+JJun0718@users.noreply.github.com>
2021-12-21 14:47:33 +0800

    Update plot_comp.py

--- plot_comp.py
+++ plot_comp.py
@@ -20,9 +20,9 @@ gcn_y_pred =  [3, 1, 0, 3, 0, 1, 3, 3, 2, 2, 0, 2, 1, 3, 2, 3, 0, 2, 1, 1, 0, 1]
 
 classes = ['0', '1', '2', '3']
 
-plot_Matrix(y_true, svm_y_pred, classes, 'Result/svm_y_pred_confusion_matrix.png')
-plot_Matrix(y_true, nb_y_pred, classes, 'Result/nb_y_pred_confusion_matrix.png')
-plot_Matrix(y_true, lr_y_pred, classes, 'Result/lr_confusion_matrix.png')
-plot_Matrix(y_true, gbdt_y_pred, classes, 'Result/gbdt_confusion_matrix.png')
-plot_Matrix(y_true, knn_y_pred, classes, 'Result/knn_confusion_matrix.png')
-plot_Matrix(y_true, gcn_y_pred, classes, 'Result/HPE_GCN_confusion_matrix.png')
+plot_Matrix(y_true, svm_y_pred, classes, 'Result/confusion_matrix/svm_confusion_matrix.png')
+plot_Matrix(y_true, nb_y_pred, classes, 'Result/confusion_matrix/nb_confusion_matrix.png')
+plot_Matrix(y_true, lr_y_pred, classes, 'Result/confusion_matrix/lr_confusion_matrix.png')
+plot_Matrix(y_true, gbdt_y_pred, classes, 'Result/confusion_matrix/gbdt_confusion_matrix.png')
+plot_Matrix(y_true, knn_y_pred, classes, 'Result/confusion_matrix/knn_confusion_matrix.png')
+plot_Matrix(y_true, gcn_y_pred, classes, 'Result/confusion_matrix/HPE_GCN_confusion_matrix.png')
